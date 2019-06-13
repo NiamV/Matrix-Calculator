fd53dabed82bafc9eac8318e582a479f964a3026
NiamV <33373416+NiamV@users.noreply.github.com>
2019-06-13 17:09:56 +0100

    Added the inverse matrix function

--- Matricies.py
+++ Matricies.py
@@ -17,7 +17,7 @@ class Matrix(object):
             row = "|    "
             for j in i:
                 noSpaces = 6 - len(str(j))
-                space = ""
+                space = "  "
                 for i in range(0,noSpaces):
                     space += " "
                 row = row + str(j) + space
@@ -26,10 +26,14 @@ class Matrix(object):
 
     #Finds the determinant of the matrix - works for any size of square matrix
     def determinant(self):
-        if len(self.entries) != len(self.entries[1]):    #Check to see if the matrix is square
+        if len(self.entries) != len(self.entries):    #Check to see if the matrix is square
             print("No determinant exists as the matrix is not square")
         else:
-            if len(self.entries) == 2:    #Determinant of a 2 by 2 matrix formula
+            if len(self.entries) == 1:
+                det = self.entries[0][0]
+                return det
+            
+            elif len(self.entries) == 2:    #Determinant of a 2 by 2 matrix formula
                 det = self.entries[0][0]*self.entries[1][1] - self.entries[1][0]*self.entries[0][1]
                 return det
             
@@ -80,10 +84,71 @@ def multiplyMatricies(Matrix1, Matrix2):
 
         return Matrix(productMatrixEntries)
 
-M1 = Matrix([[1,2],[2,1],[-1,3]])
-M2 = Matrix([[3,2,5],[-2,1,-2]])
+def Transpose(Matrix1):
+    transposeMatrixEntries = []
+    for i in range(0,Matrix1.Columns()):
+        newRow = []
+        for j in range(0,Matrix1.Rows()):
+            newRow.append(Matrix1.entries[j][i])
+        transposeMatrixEntries.append(newRow)
+    transposeMatrix = Matrix(transposeMatrixEntries)
+    return transposeMatrix
 
-print(multiplyMatricies(M1,M2).printMatrix())
+def inverseMatrix(Matrix1):
+    #Check to see if the matrix is square
+    if Matrix1.Rows() != Matrix1.Columns():    
+        return "No inverse exists as the matrix is not square"
+    else:
+        det = Matrix1.determinant()
+        size = Matrix1.Rows()
+        
+        #Check to see if the determinant of the matrix is 0
+        if det == 0:
+            return "No inverse exists as the matrix is singular"
+        else:
+            #Create matrix of cofactors
+            cofactorMatrixEntries = []
+            for i in range(0,size):         #Rows
+                nextRow = []
+                for j in range(0,size):     #Columns
+                    
+                    #Creates the minor
+                    newEntries = []
+                    for p in Matrix1.entries:
+                        row = []
+                        for q in p:
+                            row.append(q)
+                        newEntries.append(row)
+                    
+                    #Removes the jth entry in each row
+                    for row in newEntries:
+                        del row[j]
+
+                    #Removes the ith row
+                    del newEntries[i]
+
+                    Minor = Matrix(newEntries)
+                    #Divide the minors by the determinant of the original matrix
+                    cell = round(float(Minor.determinant())/float(det),3)
+
+                    #Multiplies minor by -1 if necessary to get cofactor
+                    if (i + j) % 2 == 1:
+                        cell *= -1
+
+                    nextRow.append(cell)
+                cofactorMatrixEntries.append(nextRow)
+            cofactorMatrix = Matrix(cofactorMatrixEntries)
+
+            #Transpose the cofactor matrix
+            inverse = Transpose(cofactorMatrix)
+            return inverse    
+                      
+M1 = Matrix([[1,2,4,-2],[-2,3,5,2],[2,-2,1,-1],[-2,-4,3,1]])
+
+print(inverseMatrix(M1).printMatrix())
+
+
+
 
 
 
